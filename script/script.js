0791b437961106a2f57a2dbc339efce8874e6af5
Sizon <sizon@vidrada.org>
2020-03-17 20:24:38 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const formSearch = document.querySelector('.form-search'),
+      inputCitiesFrom = formSearch.querySelector('.input__cities-from'),
+      dropdownCitiesFrom = formSearch.querySelector('.dropdown__cities-from'),
+      inputCitiesTo = formSearch.querySelector('.input__cities-to'),
+      dropdownCitiesTo = formSearch.querySelector('.dropdown__cities-to'),
+      inputDateDepart = formSearch.querySelector('.input__date-depart');
+
+      const city = ['Kyiv', 'Odessa', 'Lviv', 'Dnepr', 'Yalta', 'Rovno', 'Jitomir', 'Symmu', 'Poltava', 'Kharkiv', 'Iv-Frankovsk', 'Ternopol'];
+
+inputCitiesFrom.addEventListener('input', () => {
+  
+});
