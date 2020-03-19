4c2f5c1a8e4edf8e5d280d79d3287d8eaa46bfc9
Sizon <sizon@vidrada.org>
2020-03-19 21:47:46 +0200

    3 stream

--- script/script.js
+++ script/script.js
@@ -17,14 +17,14 @@ const showCity = (input, list) => {
 
   if (input.value !== "") {
     const filterCity = city.filter(item => {
-      const fixItem = item.name.toLowerCase();
+      const fixItem = item.name_translations.en.toLowerCase();
       return fixItem.includes(input.value.toLowerCase());
     });
 
     filterCity.forEach(item => {
       const li = document.createElement("li");
       li.classList.add("dropdown__city");
-      li.textContent = item.name;
+      li.textContent = item.name_translations.en;
       list.append(li);
     });
   }
@@ -56,6 +56,30 @@ const selectCity = (event, input, list) => {
   }
 };
 
+const renderCheapDay = (cheapTicket) => {
+  // console.log(cheapTicket);
+};
+
+const renderCheapYear = (cheapTickets) => {
+  // console.log(cheapTickets);
+};
+
+const renderCheap = (data, date) => {
+  const cheapTicketYear = JSON.parse(data).best_prices;
+  console.log(cheapTicketYear);
+
+  const cheapTicketDay = cheapTicketYear.filter((item) => {
+    return item.depart_date === date;
+  })
+  console.log(cheapTicketDay);
+
+  renderCheapDay(cheapTicketDay)
+
+  renderCheapYear(cheapTicketYear)
+};
+
+
+
 inputCitiesFrom.addEventListener("input", () => {
   showCity(inputCitiesFrom, dropdownCitiesFrom);
 });
@@ -73,6 +97,30 @@ dropdownCitiesTo.addEventListener("click", event => {
 });
 
 getData(CITIES_API, data => {
-  city = JSON.parse(data).filter(item => item.name);
-  console.log(city);
+  city = JSON.parse(data).filter(item => item.name_translations);
+  // console.log(city);
+});
+
+formSearch.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const cityFrom = city.find((item) => {
+    return inputCitiesFrom.value === item.name_translations.en
+  });
+  const cityTo = city.find((item) => {
+    return inputCitiesTo.value === item.name_translations.en
+  });
+
+  const formData = {
+    from: cityFrom.code,
+    to: cityTo.code,
+    when: inputDateDepart.value
+  };
+
+  const requestData = `?depart_date=${formData.when}&origin=${formData.from}&destination=${formData.to}&one_way=true&token=${API_KEY}`;
+
+  getData(CALENDAR + requestData, (data) => {
+    renderCheap(data, formData.when)
+  });
+
 });
